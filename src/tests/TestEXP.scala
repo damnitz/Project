0ce5cf11bd4efabe4198bdd217cede11cbf6b080
elstonla <elstonla@buffalo.edu>
2019-09-24 16:19:06 -0400

    ADD MORE TESTS

--- src/tests/TestEXP.scala
+++ src/tests/TestEXP.scala
@@ -3,20 +3,36 @@ import org.scalatest._
 import Sheet.Character
 import Sheet.Party
 class TestEXP extends FunSuite {
-  test("XP=69"){
+  test("XP=6900"){
     val char1 = new Character
     char1.EXPgain(6900)
     assert(char1.Level==6)
     assert(char1.Experience==900)
+    assert(char1.Attack==110)
+  }
+  test("XP=1"){
+    val char1 = new Character
+    char1.EXPgain(1)
+    assert(char1.Level==0)
+    assert(char1.Experience==1)
   }
   test("kill"){
     val char2= new Character
-    val char3 = new Character
-    char3.MaxHP=500
-    char3.CurrentHP=500 //2500XP
-    char2.KillCharacterGetXP(char3) // Char2 gains 2500XP EXP
-    assert(char2.Level==2)
+    val char3 = new Character//every level is 1000 xp points so char 3 gained 20 levels
+    char3.EXPgain(20000)//Char 3 now has 100+(10*20) maxhealth= 300
+    char2.KillCharacterGetXP(char3) // Char2 gains 1500XP because EXP is based on maxhealth*5
+    assert(char2.Level==1)
     assert(char2.Experience==500)//leftover xp
-    assert(char2.Attack==70)
+    assert(char2.Attack==60)
+  }
+  test("killing"){
+    val char2= new Character
+    char2.EXPgain(2500)//char2 is now level 2 with 500 leftover XP
+    val char3 = new Character//every level is 1000 xp points so char 3 gained 20 levels
+    char3.EXPgain(20000)//Char 3 now has 100+(10*20) maxhealth= 300
+    char2.KillCharacterGetXP(char3) // Char2 gains 1500XP because EXP is based on maxhealth*5
+    assert(char2.Level==4)
+    assert(char2.Experience==0)//leftover xp
+    assert(char2.Attack==90)
   }
 }
--- src/tests/TestParty.scala
+++ src/tests/TestParty.scala
@@ -14,7 +14,7 @@ class TestParty extends FunSuite{
     val char7:Character=new Character
     val char8:Character=new Character
     val party1:List[Character]=List(char1,char2,char3,char4)
-    val party2:List[Character]=List(char5,char6,char7,char8)//each char drops 500XP each,2000 total
+    val party2:List[Character]=List(char5,char6,char7,char8)//each char drops 500XP each,2000XP total
     char3.Dead=true
     char4.Dead=true//so the remaining two chars on party1 get 1000XP each
     val WinningParty:Party=new Party(party1)
@@ -40,26 +40,62 @@ class TestParty extends FunSuite{
     val char3:Character=new Character
     val char4:Character=new Character
     val char5:Character=new Character
-    char5.EXPgain(15000)//now char5 will have 250HP and will drop 1250XP as a result
+    char5.EXPgain(15000)//now char5 will have 250maxHP and will drop 1250XP as a result
     val char6:Character=new Character
     val char7:Character=new Character
     val char8:Character=new Character
     val party1:List[Character]=List(char1,char2,char3,char4)
     val party2:List[Character]=List(char5,char6,char7,char8)//each char drops 500XP each with the exception of char5 who drops 1250XP,2750 total
+    char2.Dead=true//now only char1 is alive
     char3.Dead=true
-    char4.Dead=true//so the remaining two chars on party1 get 1375XP each
+    char4.Dead=true//so the remaining char left alive will gain 2750XP
     val WinningParty:Party=new Party(party1)
     val LosingParty:Party=new Party(party2)
     WinningParty.DefeatParty(LosingParty)
-    assert(char1.Level==1)//every 1000XP=LevelUP
-    assert(char2.Level==1)
-    assert(char1.Experience==375)//leftover XP
-    assert(char2.Experience==375)
-    assert(char1.Attack==60)//Every level you gain +10 to stats
-    assert(char2.Attack==60)
+    assert(char1.Level==2)//every 1000XP=LevelUP
+
+    assert(char1.Experience==750)//leftover XP
+
+    assert(char1.Attack==70)//Every level you gain +10 to stats
+
+    assert(char2.Level==0)
     assert(char3.Level==0)//dead so didnt gain XP
     assert(char4.Level==0)//dead
+
+    assert(char2.Experience==0)
     assert(char3.Experience==0)//dead
     assert(char4.Experience==0)//dead
+  }
+  test("party3!"){
+    val char1:Character=new Character
+    val char2:Character=new Character
+    val char3:Character=new Character
+    val char4:Character=new Character
+    val char5:Character=new Character
+    char5.EXPgain(15000)//now char5 will have 250maxHP and will drop 1250XP as a result
+    val char6:Character=new Character
+    val char7:Character=new Character
+    val char8:Character=new Character
+    val party1:List[Character]=List(char1,char2,char3,char4)
+    val party2:List[Character]=List(char5,char6,char7,char8)//each char drops 500XP each with the exception of char5 who drops 1250XP,2750 total
+    //nobody is dead, everyone gains 687.5XP(rounded down)
+    val WinningParty:Party=new Party(party1)
+    val LosingParty:Party=new Party(party2)
+    WinningParty.DefeatParty(LosingParty)
+    assert(char1.Level==0)//every 1000XP=LevelUP
+    assert(char2.Level==0)
+    assert(char3.Level==0)
+    assert(char4.Level==0)
+
+    assert(char1.Experience==687)//leftover XP
+    assert(char2.Experience==687)
+    assert(char3.Experience==687)
+    assert(char4.Experience==687)
+
+    assert(char1.Attack==50)//Every level you gain +10 to stats
+    assert(char2.Attack==50)
+    assert(char3.Attack==50)
+    assert(char4.Attack==50)
+
   }
 }
